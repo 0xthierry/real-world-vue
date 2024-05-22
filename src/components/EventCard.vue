38548c2795d25bf5fbbddcbb6fa3737515f975ca
Thierry <thierrysantoos123@gmail.com>
2024-05-21 21:41:55 -0300

    feat: create vue app

---
+++ src/components/EventCard.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+  event: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="event-card">
+    <h2>{{ event.title }}</h2>
+    <span>{{ event.time }} on {{ event.date }}</span>
+  </div>
+</template>
+<!--Because style has this scoped property these styles will only be applied to this component-->
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
